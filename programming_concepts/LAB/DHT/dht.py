0a029d6f6bfab394d145a64f6389abafe69c2bec
job nganga <jobugua97@gmail.com>
2024-11-13 02:00:20 +0300

    new on dht

--- programming_concepts/LAB/DHT/dht.py
+++ programming_concepts/LAB/DHT/dht.py
@@ -7,7 +7,7 @@ pin = 18
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 if humidity is not None and temperature is not None:
-    print(f"Temperature: {temperature} C")
-    print(f"Humidity: {humidity}%")
+    print(temperature)
+    print(humidity)
 else:
     print("Failed to retrieve data from humidity sensor")
