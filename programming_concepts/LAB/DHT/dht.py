5ac96867b45a418f7db3f3889997421dbf75131b
job nganga <jobugua97@gmail.com>
2024-11-13 02:19:19 +0300

    new on dht

--- programming_concepts/LAB/DHT/dht.py
+++ programming_concepts/LAB/DHT/dht.py
@@ -1,13 +1,24 @@
-import Adafruit_DHT
+import Adafruit_DHT 
 
-# Set the sensor type and GPIO pin
-sensor = Adafruit_DHT.DHT11
-pin = 18  # GPIO pin number
+def readings(self):
+    DHT_SENSOR = Adafruit_DHT.DHT11 # initialises the variable  DHT_SENSOR to specifically hold values from the DHT11 sensor from the Adafruit_DHT module
+    DHT1_PIN1 = 18 # initiates DHT_PIN1 to pin 18 of the raspberry pi
+    DHT2_PIN2 = 3 #initaites  DHT_PIN2 to pin 3 of the raspberry pi
+    try:
+        try:
+            humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT1_PIN1) 
+            return (humidity,temperature)
+        except:
+            humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT2_PIN2)
+            return (humidity,temperature)
+    except:
+        return None
 
-humidity, temperature = Adafruit_DHT.read(sensor,pin)
-
-if humidity is not None and temperature is not None:
-    print(temperature)
-    print(humidity)
-else:
-    print("Failed to retrieve data from humidity sensor")
+while True:
+    data=readings()
+    if data is None:
+        message="check the dht's"
+        print(message)
+    else:
+        message=readings
+        print(message)
